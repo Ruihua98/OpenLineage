a0da6606c254fdbc2a8801ed0178dfe1df06c444
Ruihua Wang <ruihuawang@microsoft.com>
2024-01-16 15:13:55 +0800

    Add a new construction method for SparkPropertyFacet

--- integration/spark/shared/src/main/java/io/openlineage/spark/agent/facets/SparkPropertyFacet.java
+++ integration/spark/shared/src/main/java/io/openlineage/spark/agent/facets/SparkPropertyFacet.java
@@ -9,11 +9,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.openlineage.client.OpenLineage;
 import io.openlineage.spark.agent.Versions;
 import java.util.Map;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.SparkContext;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.HashMap;
+import org.apache.spark.SparkConf;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class SparkPropertyFacet extends OpenLineage.DefaultRunFacet {
   @JsonProperty("properties")
   @SuppressWarnings("PMD")
   private Map<String, Object> properties;
+  private static final ALLOWED_KEY = "spark.openlineage.capturedProperties";
 
   public Map<String, Object> getProperties() {
     return properties;
@@ -22,5 +31,28 @@ public class SparkPropertyFacet extends OpenLineage.DefaultRunFacet {
   public SparkPropertyFacet(Map<String, Object> environmentDetails) {
     super(Versions.OPEN_LINEAGE_PRODUCER_URI);
     this.properties = environmentDetails;
+  }
+
+  public SparkPropertyFacet(){
+    super(Versions.OPEN_LINEAGE_PRODUCER_URI);
+    properties=new HashMap<>();
+    try{
+      SparkSession session = SparkSession.active();
+      Set<String> allowerProperties = new HashSet<>();
+      SparkContext context = session.sparkContext();
+      SparkConf conf = context.getConf();
+
+      if(conf.contains(ALLOWED_KEY)){
+        allowerProperties = conf.get(ALLOWED_KEY).split(",").collect(Collectors.toSet());
+      }
+
+      for(String key: allowerProperties){
+        if(null != session.conf().get(key)){
+          properties.putIfAbsent(key, session.conf().get(key));
+        }
+      }
+    }catch(IllegalStateException){
+      properties=new HashMap<>();
+    }
   }
 }
